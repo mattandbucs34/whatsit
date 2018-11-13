0d4c2e42113559edff89c8c4c40f8a9c72c8b4b1
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2018-11-12 19:59:44 -0700

    WIP deploy to production

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 /* app.use("/", (req, res, next) => {
   res.send("Welcome to Bloccit")
 }); */
 
+appConfig.init();
 routeConfig.init(app);
 
 module.exports = app;
