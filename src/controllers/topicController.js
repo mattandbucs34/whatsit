54f70e591e90fb49117291df6e124b7f7915ec96
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2018-11-20 22:01:11 -0700

    chkpt topic resource 1 files added

---
+++ src/controllers/topicController.js
@@ -0,0 +1,11 @@
+module.exports = {
+  index(req, res, next) {
+    topicQueries.getAllTopics((err, topics) => {
+      if(err) {
+        res.redirect(500, "static/index");
+      }else {
+        res.render("topics/index", {topics});
+      }
+    })
+  }
+}
