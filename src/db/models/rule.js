fb1ed91af18fe187f7908e0c955875c78b0191e2
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2018-11-21 09:35:05 -0700

    assignment topic-resource-1

---
+++ src/db/models/rule.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Rule = sequelize.define('Rule', {
+    description: DataTypes.STRING,
+    topicId: {
+      type: Sequelize.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topic",
+        key: "id",
+        as: "topicId",
+      }
+    }
+  }, {});
+  Rule.associate = function(models) {
+    // associations can be defined here
+    Rule.belongsTo(models.Topic, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE",
+    });
+  };
+  return Rule;
+};
