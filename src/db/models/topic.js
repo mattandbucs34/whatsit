eb6c1bc1195707d2e634c85a860bbbe682de589b
mattandbucs34 <35737262+mattandbucs34@users.noreply.github.com>
2018-11-25 10:57:11 -0700

    chkpt bloccit-post first post

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,8 +1,14 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {});
   Topic.associate = function(models) {
     // associations can be defined here
@@ -19,6 +25,11 @@ module.exports = (sequelize, DataTypes) => {
     Topic.hasMany(models.Advert, {
       foreignKey: "topicId",
       as: "adverts",
+    });
+
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicId",
+      as: "posts"
     });
   };
   return Topic;
